d59f194e59c974a16f2e5f2b1694fe9458df22d3
lukaszub <44746812+lukaszub@users.noreply.github.com>
2022-12-18 21:40:03 +0100

    #35 Added phone number and surname field to user table by migration

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -7,8 +7,9 @@
       <tr>
         <th scope="col">#</th>
         <th scope="col">E-mail</th>
-        <th scope="col">Name</th>
-        <th scope="col">Surname</th>
+        <th scope="col">Imię</th>
+        <th scope="col">Nazwisko</th>
+        <th scope="col">Numer telefonu</th>
         <th scope="col">Akcje</th>
       </tr>
     </thead>
@@ -18,7 +19,8 @@
           <th scope="row">{{ $user->id }}</th>
           <td>{{ $user->email }}</td>
           <td>{{ $user->name }}</td>
-          <td>@mdo</td>
+          <td>{{ $user->surname }}</td>
+          <td>{{ $user->phone_number }}</td>
           <td></td>
         </tr>        
       @endforeach
