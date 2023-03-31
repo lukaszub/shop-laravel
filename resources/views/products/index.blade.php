b7d84e063288d7894d3f5996c9bdaca70fc8b387
lukaszub <44746812+lukaszub@users.noreply.github.com>
2023-03-31 19:17:24 +0200

    Fontawesome

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -5,11 +5,11 @@
   @include('helpers.flash_messages')
   <div class="row">
     <div class="col-6">
-      <h1>Lista produktów</h1>
+      <h1><i class="fa-solid fa-table-list"></i> Lista produktów</h1>
     </div>
     <div class="col-6">
       <a class="float-end" href="{{ route('products.create') }}">
-        <button type="button"class="btn btn-primary">Dodaj</button>
+        <button type="button"class="btn btn-primary"><i class="fa-solid fa-square-plus"></i></button>
       </a>    
     </div>
   </div>
@@ -38,12 +38,12 @@
       
             <td>
               <a href="{{ route('products.show', $product->id ) }}">
-                <button class="btn btn-primary btn-sm">P</button>
+                <button class="btn btn-primary btn-sm"><i class="fa-solid fa-magnifying-glass"></i></button>
               </a>
               <a href="{{ route('products.edit', $product->id ) }}">
-                <button class="btn btn-success btn-sm">E</button>
+                <button class="btn btn-success btn-sm"><i class="fa-regular fa-pen-to-square"></i></button>
               </a>
-              <button class="btn btn-danger btn-sm delete" data-id="{{ $product->id }}">X</button>
+              <button class="btn btn-danger btn-sm delete" data-id="{{ $product->id }}"><i class="fa-solid fa-trash"></i></button>
             </td>
           </tr>        
         @endforeach
